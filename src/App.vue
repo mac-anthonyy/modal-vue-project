e54d57124efb584781472bb03661fde16bcd931c
mac-anthony <ekenemacanthony@gmail.com>
2024-08-09 12:49:41 +0100

    Add .gitattributes to manage line endings and binary files

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <h1> {{pageTitle}}</h1>
+  <h3>Welcome....</h3>
+  <div v-if="showModal">
+      <!-- <Modal 
+        :modalTitle="modalTitle" 
+        :modalText="modalText" 
+        theme="sale" 
+        @close-modal="toggleModal" /> -->
+
+        <Modal theme="sale" @close-modal="toggleModal" >
+          <h1>Cash Giveaway</h1>
+          <p>Win cash and prizes</p>
+          <template v-slot:links>
+            <a href="#">Sign Up</a>
+            <a href="#">Sign In</a>
+          </template>
+        </Modal>
+  </div>
+
+  <teleport to='.modals'  v-if="showModalTwo">
+    <Modal @close-modal="toggleModalTwo">
+        <h1>Modal 2</h1>
+        <p>small content for modal 2</p>
+       
+    </Modal>
+  </teleport>
+
+  <button @click="toggleModal">Open Modal</button>
+  <button @click="toggleModalTwo">Open Modal2</button>
+</template>
+
+<script>
+
+import Modal from './components/Modal'
+
+export default {
+  name: 'App',
+  components: {
+    Modal
+  },
+  data(){
+    return {
+      pageTitle: 'Modal Pop Up Project',
+      modalTitle: 'Sign up for the giveaway',
+      modalText: 'Grab Your ninja swag fro half the price',
+      showModal: false,
+      showModalTwo: false
+    }
+  },
+  methods: {
+      toggleModal(){
+        this.showModal = !this.showModal;
+      },
+      toggleModalTwo(){
+        this.showModalTwo = !this.showModalTwo
+    },
+}
+}
+</script>
+
+<style>
+#app, .modals {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+button{
+  margin: 10px;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,73 @@
+
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{sale: theme === 'sale'}">
+            <!-- we can pass in a default content in the general slot and it will only show if default content is not passed in -->
+            <slot>Default Content</slot>
+            <!-- for named slots -->
+             <div>
+                <slot name="links"></slot>
+             </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['modalTitle','modalText', 'theme'],
+    methods: {
+       closeModal(){
+        this.$emit('close-modal');
+       }
+    }
+}
+</script>
+
+<style>
+.modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+}
+.backdrop{
+    top: 0;
+    position: fixed;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+}
+.modal h1{
+    color: #03cfb4;
+    border: none;
+    padding: 0;
+}
+.modal .actions {
+    text-align: center;
+    margin: 30px 0 10px 0;
+}
+.modal .actions a {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 8px;
+    text-decoration: none;
+    margin: 10px;
+}
+.modal.sale {
+    background: #dc143c;
+    color: #fff;
+}
+.modal.sale h1{
+    color: #fff;
+}
+
+.modal.sale .actions{
+    color: #fff;
+}
+.modal.sale .actions a{
+    color: #fff;
+}
+</style>
